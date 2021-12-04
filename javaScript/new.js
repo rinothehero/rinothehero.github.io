c426392d42e7e86a03be166ab25b61352c4b7c03
Rino <rinothehero@gmail.com>
2021-12-05 01:41:58 +0900

    website version 1.0

---
+++ javaScript/new.js
@@ -0,0 +1,8 @@
+$('a[href^="#"]').on("click", function (e) {
+    e.preventDefault();
+    var target = $($(this).attr("href"));
+
+    if (target.length) {
+        $("html, body").animate({ scrollTop: target.offset().top }, 400, "easeInOutQuad");
+    }
+});
